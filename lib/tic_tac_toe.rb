df919c43636544fe594c9634368aa607bd6fe62a
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 20:18:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,6 +12,7 @@ class TicTacToe
   ]
   
   def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end
   
   def current_player
@@ -96,14 +97,16 @@ end
 end
   
   
-  def play
-    @board = Array.new(9, " ")
-
-    turn until over?
-    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
-    puts "Would you like to play again? (Y or N)"
-    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
+def play
+  while over? == false
+    turn
   end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
  
  
   def display_board
