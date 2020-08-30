30fa5389b2bd7bdcbfd0e1f17d2853e7ab63636d
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 20:17:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,16 +97,17 @@ end
 end
   
   
-def play
-  while over? == false
-    turn
+  def play
+    @board = Array.new(9, " ")
+    # Play until someone wins or there is a draw
+    turn until over?
+    # Congratulate the winner
+    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
+    # Ask if they'd like to play again
+    puts "Would you like to play again? (Y or N)"
+    # If yes, then #play again
+    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
   end
-  if won?
-    puts "Congratulations #{winner}!"
-  elsif draw?
-    puts "Cat's Game!"
-  end
-end
  
  
   def display_board
